e87fac66869226e47120d6b7df6ffd84548c77a0
logarythm <mahmood.ihsan98989@gmail.com>
2023-03-17 17:44:54 +0000

    added padding changed text color added link to icons when clicked

--- src/Components/skills.js
+++ src/Components/skills.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { DiHtml5, DiCss3, DiBootstrap, DiJavascript, DiGit, DiReact} from "react-icons/di"
+import { DiHtml5, DiCss3, DiBootstrap, DiJavascript, DiGit, DiReact, DiNpm, DiNodejs} from "react-icons/di"
 
 const Skills = ()=>{
 
@@ -7,7 +7,7 @@ const Skills = ()=>{
         width:"85%"
     }
     const csspercent = {
-        width:"72%"
+        width:"55%"
     }
     const bootstrappercent = {
         width:"77%"
@@ -21,48 +21,50 @@ const Skills = ()=>{
     
     
     return(
-        <div className="flex flex-wrap">
-            <div className="w-1/2">
-                <p>html</p> 
+        <div className="flex flex-wrap py-5">
+            <div className="w-full lg:w-1/2">
+                <p className="text-white">html</p> 
                 <div class="w-full bg-gray-200 rounded-full h-2.5">
                     <div class="bg-percent-bar h-2.5 rounded-full" style={htmlpercent}> 
                     </div>
                 </div>
 
-                <p>css</p>
+                <p className="text-white">css</p>
                 <div class="w-full bg-gray-200 rounded-full h-2.5">
                     <div class="bg-percent-bar h-2.5 rounded-full" style={csspercent}> 
                     </div>
                 </div>
 
-                <p>bootstrap</p>
+                <p className="text-white">bootstrap</p>
                 <div class="w-full bg-gray-200 rounded-full h-2.5">
                     <div class="bg-percent-bar h-2.5 rounded-full" style={bootstrappercent}> 
                     </div>
                 </div>
 
-                <p>javascript</p>
+                <p className="text-white">javascript</p>
                 <div class="w-full bg-gray-200 rounded-full h-2.5">
                     <div class="bg-percent-bar h-2.5 rounded-full" style={jspercent}> 
                     </div>
                 </div>
 
-                <p>react</p>
+                <p className="text-white">react</p>
                 <div class="w-full bg-gray-200 rounded-full h-2.5">
                     <div class="bg-percent-bar h-2.5 rounded-full" style={reactpercent}> 
                     </div>
                 </div>
             </div>
 
-            <div className="w-1/2">
-                <p className="text-white text-center">my tech stack</p>
+            <div className="w-full lg:w-1/2">
+                <p className="text-white text-center py-5">my tech stack</p>
                 <div className="w-full flex flex-wrap justify-between">
-                    <DiHtml5 className="text-6xl"/>
-                    <DiCss3 className="text-6xl"/>
-                    <DiJavascript className="text-6xl"/>
-                    <DiBootstrap className="text-6xl"/>
-                    <DiGit className="text-6xl"/>
-                    <DiReact className="text-6xl"/>
+                    <DiHtml5 className="text-6xl hover:fill-orange-500 hover:cursor-pointer" onClick={()=>window.open("https://html.com/html5/", "_blank")}/>
+                    <DiCss3 className="text-6xl hover:fill-blue-600 hover:cursor-pointer" onClick={()=>window.open("https://www.w3schools.com/css/", "_blank")}/>
+                    <DiJavascript className="text-6xl hover:fill-yellow-500 hover:cursor-pointer" onClick={()=>window.open("https://www.javascript.com/", "_blank")}/>
+                    <DiBootstrap className="text-6xl hover:fill-purple-500 hover:cursor-pointer" onClick={()=>window.open("https://getbootstrap.com/", "_blank")}/>
+                    <DiGit className="text-6xl hover:fill-orange-600 hover:cursor-pointer" onClick={()=>window.open("https://git-scm.com/", "_blank")}/>
+                    <DiReact className="text-6xl hover:fill-blue-200 hover:cursor-pointer" onClick={()=>window.open("https://react.dev/", "_blank")}/>
+                    <DiNpm className="text-6xl hover:fill-red-500 hover:cursor-pointer" onClick={()=>window.open("https://www.npmjs.com/", "_blank")}/>
+                    <DiNodejs className="text-6xl hover:fill-green-500 hover:cursor-pointer" onClick={()=>window.open("https://nodejs.org/en", "_blank")}/>
                     
                 </div>
             </div>
