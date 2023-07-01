34e2dc3d7d14aa121d28b51501382470e79e5b46
logarythm <mahmood.ihsan98989@gmail.com>
2023-07-01 04:53:27 +0100

    added functional contact section

--- src/Components/contact.js
+++ src/Components/contact.js
@@ -1,11 +1,86 @@
-import React from "react";
+import React, { useState } from 'react';
 
-const Contact = ()=>{
-    return(
-        <div>
-            <p>contact section</p>
-        </div>
-    )
-}
+const ContactForm = () => {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [message, setMessage] = useState('');
 
-export default Contact
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    // Send the form data to Formspree
+    const response = await fetch('https://formspree.io/f/xwkdeenw', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ name, email, message }),
+    });
+
+    if (response.ok) {
+      // Clear the form fields on successful submission
+      setName('');
+      setEmail('');
+      setMessage('');
+      alert('Message sent!');
+    } else {
+      alert('Message failed to send. Please try again.');
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit} className="max-w-lg mx-auto" id="contact">
+      <div className="mb-4">
+        <label htmlFor="name" className="block text-gray-700 text-sm font-bold mb-2">
+          Name
+        </label>
+        <input
+          type="text"
+          id="name"
+          className="w-full p-2 border border-gray-300 rounded"
+          placeholder="Your Name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          required
+        />
+      </div>
+      <div className="mb-4">
+        <label htmlFor="email" className="block text-gray-700 text-sm font-bold mb-2">
+          Email
+        </label>
+        <input
+          type="email"
+          id="email"
+          className="w-full p-2 border border-gray-300 rounded"
+          placeholder="Your Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          required
+        />
+      </div>
+      <div className="mb-4">
+        <label htmlFor="message" className="block text-gray-700 text-sm font-bold mb-2">
+          Message
+        </label>
+        <textarea
+          id="message"
+          className="w-full p-2 border border-gray-300 rounded"
+          placeholder="Your Message"
+          rows="4"
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+          required
+        />
+      </div>
+      <button
+        type="submit"
+        className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded"
+      >
+        Send Message
+      </button>
+    </form>
+  );
+};
+
+export default ContactForm;
+
