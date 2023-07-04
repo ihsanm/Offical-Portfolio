28518ca4ce2d57428b5bf61defdf9b7727fced33
logarythm <mahmood.ihsan98989@gmail.com>
2023-07-04 16:47:57 +0100

    added functionality to navbar

--- src/Components/navbar.js
+++ src/Components/navbar.js
@@ -1,40 +1,61 @@
-import React, { useState } from "react"
-import { Link } from "react-router-dom"
-import logo from "../assets/Logo.png"
+import React from "react";
+import { Link, animateScroll as scroll } from "react-scroll";
+import logo from "../assets/Logo.png";
 
-const Navbar = ()=> {
-    const [active, setActive] = useState("")
-    return(
-        <nav className="w-full flex items-center py-1 top-0 z-20 bg-ihsan-blue sticky">
-            <div className="w-full flex justify-between items-center max-w-7xl mx-auto">
-                <Link
-                to="/"
-                className="flex items-center gap-2"
-                onClick={() => {
-                    setActive("");
-                    window.scrollTo(0,0);
-                }}>
-                    <img src={logo} alt="logo" className="w-14 h-14 object-contain"/>
-                    <p className="text-white text-[18px] font-bold cursor-pointer hidden lg:flex">Ihsan Mahmood</p>
-                </Link>
-                <Link to="/">
-                    <p className="text-white flex items-center gap-2">Home</p>
-                </Link>
-                <Link to="/skills">
-                    <p className="text-white flex items-center gap-2">Skills</p>
-                </Link>
-                <Link to="/projects">
-                    <p className="text-white flex items-center gap-2">Projects</p>
-                </Link>
-                <Link to="/contact">
-                <p className="text-white flex items-center gap-2 px-2 ">Lets connect!</p>
-                </Link>
+const Navbar = () => {
+  const handleLinkClick = () => {
+    scroll.scrollToTop({
+      smooth: true,
+      duration: 500,
+    });
+  };
 
-            </div>
+  return (
+    <nav className="w-full flex items-center py-1 top-0 z-20 bg-ihsan-blue sticky">
+      <div className="w-full flex justify-between items-center max-w-7xl mx-auto">
+        <Link to="/" className="flex items-center gap-2" onClick={handleLinkClick}>
+          <img src={logo} alt="logo" className="w-14 h-14 object-contain" />
+          <p className="text-white text-18 font-bold cursor-pointer hidden lg:flex">Ihsan Mahmood</p>
+        </Link>
+        <Link
+          to="home"
+          smooth={true}
+          duration={500}
+          className="text-white flex items-center gap-2 hover:text-gray-300 cursor-pointer"
+        >
+          Home
+        </Link>
+        <Link
+          to="skills"
+          smooth={true}
+          duration={500}
+          className="text-white flex items-center gap-2 hover:text-gray-300 cursor-pointer"
+        >
+          Skills
+        </Link>
+        <Link
+          to="projects"
+          smooth={true}
+          duration={500}
+          className="text-white flex items-center gap-2 hover:text-gray-300 cursor-pointer"
+        >
+          Projects
+        </Link>
+        <Link
+          to="contact"
+          smooth={true}
+          duration={500}
+          className="text-white flex items-center gap-2 px-2 hover:text-gray-300 cursor-pointer"
+        >
+          Let's connect!
+        </Link>
+      </div>
+    </nav>
+  );
+};
 
-        </nav>
-    )
+export default Navbar;
 
-}
 
-export default Navbar 
+
+
